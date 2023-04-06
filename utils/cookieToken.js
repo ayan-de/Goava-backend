45ea8c7a18b7643237336e3345c152b799045a03
ayan-de <deayan252@gmail.com>
2023-04-07 03:47:06 +0530

    "cookie token setup for future hosting"

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -5,7 +5,9 @@ const cookieToken = (user, res, message) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_TIME * 24 * 60 *60 *1000
         ),
-        httpOnly: true,
+        httpOnly: true, //accessible only by web server
+        // secure: true, //https
+        // sameSite:'None' //cross-site cookie
     }
 
     user.password = undefined;
